113d96ed58554b043d6fa208aeb9e01f250a6fed
lee1043 <lee1043@llnl.gov>
2022-04-27 17:11:34 -0700

    clean up

--- scripts/collect_test_case.sh
+++ scripts/collect_test_case.sh
@@ -1,3 +1,10 @@
+# Collect individual model json file for user-model visualization demo
+
 # mean climate
 
-cp /p/user_pub/pmp/pmp_results/pmp_v1.1.2/metrics_results/mean_climate/cmip6/historical/v20201217/*/*E3SM-1-0*.v????????.json ../test_case/mean_climate/
+mip="cmip6"
+exp="historical"
+model="E3SM-1-0"
+version="v20201008"
+
+cp /p/user_pub/pmp/pmp_results/pmp_v1.1.2/metrics_results/mean_climate/$mip/$exp/${version}/*/*${model}*.${version}.json ../test_case/mean_climate/
